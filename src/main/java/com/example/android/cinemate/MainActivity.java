9379c4c11ebc05fa7dacb2ce7d8a0bfe83e0f4e7
Gavin <gav.plant@gmail.com>
2017-07-31 22:35:40 +1200

    recyclerview loaded

---
+++ src/main/java/com/example/android/cinemate/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.cinemate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private MovieAdapter mMovieAdapter;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        ArrayList<String> movies = new ArrayList<>();
+        movies.add("Scarface");
+        movies.add("Casino");
+        movies.add("Pulp Fiction");
+        movies.add("Oldboy");
+        movies.add("Jackie Brown");
+        movies.add("From Dusk till Dawn");
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mMovieAdapter = new MovieAdapter(movies);
+        mRecyclerView.setAdapter(mMovieAdapter);
+    }
+}
---
+++ src/main/java/com/example/android/cinemate/MovieAdapter.java
@@ -0,0 +1,49 @@
+package com.example.android.cinemate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gavin on 31-Jul-17.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    ArrayList<String> mMovies;
+
+    public MovieAdapter(ArrayList<String> movies) {
+        mMovies = movies;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mMovies.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView.findViewById(R.id.moviesTextView);
+        }
+    }
+}
