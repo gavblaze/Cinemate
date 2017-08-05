e67d20e6e7bc8dc9cf03b53882e7d82f7a8f8341
Gavin <gav.plant@gmail.com>
2017-08-06 01:16:00 +1200

    add picasso dependancy

--- src/main/java/com/example/android/cinemate/MovieAdapter.java
+++ src/main/java/com/example/android/cinemate/MovieAdapter.java
@@ -1,11 +1,15 @@
 package com.example.android.cinemate;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
--- src/main/java/com/example/android/cinemate/SettingsActivity.java
+++ src/main/java/com/example/android/cinemate/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -37,11 +38,30 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref);
+
+
+            Preference sortOrder = findPreference(getString(R.string.sort_order_key));
+            bindPreferenceSummaryToValue(sortOrder);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setValue(o.toString());
+            listPreference.setSummary(listPreference.getEntry());
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = sharedPreferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
