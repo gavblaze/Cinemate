0ee766146dacfa830583f01faa8f26fa9ad2e36f
Gavin <gav.plant@gmail.com>
2017-08-19 23:22:53 +1200

    add favourites to preference screen

--- src/main/java/com/example/android/cinemate/DetailActivity.java
+++ src/main/java/com/example/android/cinemate/DetailActivity.java
@@ -65,7 +65,6 @@ public class DetailActivity extends AppCompatActivity {
             mRatingTextView.setText(mMovie.getmVoteAverage());
 
             Picasso.with(context).invalidate(posterUrl);
-
         }
 
 
